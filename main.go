ea7767bf51fe4035991953d2836c13ae0e0ddee6
Josias Chocooj <jchocooj@katch.com>
2017-11-21 14:41:18 -0600

    Add initial routes and functions to CRUD data
    '

--- main.go
+++ main.go
@@ -11,10 +11,18 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	return
 }
+func GetPeople(w http.ResponseWriter, r *http.Request)    {}
+func GetPerson(w http.ResponseWriter, r *http.Request)    {}
+func CreatePerson(w http.ResponseWriter, r *http.Request) {}
+func DeletePerson(w http.ResponseWriter, r *http.Request) {}
 
 func main() {
 	fmt.Println("Hello World!")
 	router := mux.NewRouter()
 	router.HandleFunc("/", handler)
+	router.HandleFunc("/people", GetPeople).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
